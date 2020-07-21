673abeecd608bacda3d3ea98f076104f2bf83868
Yuan Yao <adayuanyao@gmail.com>
2020-07-21 16:39:03 -0700

    Finished NewsCard

---
+++ v1-newsClickCard/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './navbar';
+import News from './news';
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<Navbar />
+			<News />
+		</div>
+	);
+}
+
+export default App;
