0d8bfa5483c090974addab17c5edb67612a021a2
Yuan Yao <adayuanyao@gmail.com>
2020-08-03 11:09:21 -0700

    Update style of news components

--- v1-newsClickCard/src/news.js
+++ v1-newsClickCard/src/news.js
@@ -10,8 +10,8 @@ class News extends React.Component {
 		let news = require('./news.json');
 		return (
 			<div className="News">
-				<h1>COVID-19 NEWS</h1>
-				<h5>Click the card to view the content</h5>
+				<h1 style={{ textAlign: 'center', color: 'white' }}>COVID-19 NEWS</h1>
+				<h5 style={{ textAlign: 'center', color: 'white' }}>Click the card to view the content</h5>
 				<div className="News">{ShowNews(news)}</div>
 			</div>
 		);
