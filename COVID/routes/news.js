6ff66637c2dfeacfb815bea423dcca41cb989281
KemengXu <kemengxu@gmail.com>
2020-08-11 23:18:11 -0700

    modified back

--- COVID/routes/news.js
+++ COVID/routes/news.js
@@ -2,12 +2,22 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 router.get('/', function(req, res, next) {
-    fs.watch("./routes/newsSpider/News.json", function () {
-        try{
-            let news = require('../routes/newsSpider/News.json');
-            res.send(JSON.stringify(news));
-        } catch (e) {}
-    });
+    try{
+        let news = require('../routes/newsSpider/News.json');
+        console.log("news")
+        res.send(JSON.stringify(news));
+    } catch (e) {
+        console.log("Working...")
+    }
+    // fs.watch("./routes/newsSpider/News.json", function () {
+    //     try{
+    //         let news = require('../routes/newsSpider/News.json');
+    //         console.log("news")
+    //         res.send(JSON.stringify(news));
+    //     } catch (e) {
+    //         console.log("Working...")
+    //     }
+    // });
 });
 
 module.exports = router;
--- COVID/routes/newsSpider/pyRun.py
+++ COVID/routes/newsSpider/pyRun.py
@@ -1,9 +1,11 @@
 import sys
 from scrapy import cmdline
+import os
 
-f = open('News.json', 'r+')
-f.truncate(0) # need '0' when using r+
-f.close()
+if os.path.exists("News.json"):
+  os.remove("News.txt")
+else:
+  print("The file does not exist")
 cmdline.execute("scrapy crawl news -o News.json".split())
 
 sys.stdout.flush()
